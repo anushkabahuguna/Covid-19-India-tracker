2f22d12a0ea270ae926e1063032ec37bde65b97d
Anushka Bahuguna <anushkabahugunaa@gmail.com>
2021-01-15 21:06:23 +0530

    change js 2

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ $(document).ready(() => {
 	$.getJSON('https://covid-api.mmediagroup.fr/v1/cases?country=India')
 		.done((res) => {
 			console.log(res['All']['confirmed']);
-			$('.confirmed ').text(res['All']['confirmed'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
-			$('.recovered ').text(res['All']['recovered'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
-			$('.deaths ').text(res['All']['deaths'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
-			$('.current-pop ').text(res['All']['population'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
-			$('.life-expectancy ').text(res['All']['life_expectancy'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+			$('.two .confirmed ').text(res['All']['confirmed'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+			$('.two .recovered ').text(res['All']['recovered'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+			$('.two .deaths ').text(res['All']['deaths'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+			$('.two .current-pop ').text(res['All']['population'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+			$('.two .life-expectancy ').text(
+				res['All']['life_expectancy'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,')
+			);
 		})
 		.fail((err) => {});
 });
