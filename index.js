33cd514a477d3d65257f3a59721f1b5fc1a61a99
Anushka Bahuguna <anushkabahugunaa@gmail.com>
2021-01-15 21:05:22 +0530

    change index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 $(document).ready(() => {
-	// $.getJSON('https://covid-api.mmediagroup.fr/v1/cases?country=India')
-	// 	.done((res) => {
-	// 		console.log(res['All']['confirmed']);
-	// 		$('.confirmed span').text(res['All']['confirmed'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
-	// 		$('.recovered span').text(res['All']['recovered'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
-	// 		$('.deaths span').text(res['All']['deaths'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
-	// 		$('.current-pop span').text(res['All']['population'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
-	// 		$('.life-expectancy span').text(
-	// 			res['All']['life_expectancy'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,')
-	// 		);
-	// 	})
-	// 	.fail((err) => {});
+	$.getJSON('https://covid-api.mmediagroup.fr/v1/cases?country=India')
+		.done((res) => {
+			console.log(res['All']['confirmed']);
+			$('.confirmed ').text(res['All']['confirmed'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+			$('.recovered ').text(res['All']['recovered'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+			$('.deaths ').text(res['All']['deaths'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+			$('.current-pop ').text(res['All']['population'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+			$('.life-expectancy ').text(res['All']['life_expectancy'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+		})
+		.fail((err) => {});
 });
