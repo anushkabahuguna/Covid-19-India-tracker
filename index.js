839b4505499e20490524a92867ce48ac41252eea
Anushka Bahuguna <anushkabahugunaa@gmail.com>
2021-01-15 21:02:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+	// $.getJSON('https://covid-api.mmediagroup.fr/v1/cases?country=India')
+	// 	.done((res) => {
+	// 		console.log(res['All']['confirmed']);
+	// 		$('.confirmed span').text(res['All']['confirmed'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+	// 		$('.recovered span').text(res['All']['recovered'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+	// 		$('.deaths span').text(res['All']['deaths'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+	// 		$('.current-pop span').text(res['All']['population'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,'));
+	// 		$('.life-expectancy span').text(
+	// 			res['All']['life_expectancy'].toString().replace(/(\d)(?=(\d\d)+\d$)/g, '$1,')
+	// 		);
+	// 	})
+	// 	.fail((err) => {});
+});
